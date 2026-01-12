63193f2d223282c19dfd5d06f9ac670492cc67da
Grisha Kuznetsov <exolins@yandex.ru>
2026-01-12 14:08:12 +0300

    add json files

---
+++ gendiff/diff/file_read.py
@@ -0,0 +1,5 @@
+#file_read
+import json
+
+def read_json(file_path):
+    return json.load(open(file_path))
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,4 +1,9 @@
 import argparse
+import json
+def read_json(file_path):
+    return json.load(open(file_path))
+# def file_get():
+#     file = read_json('gendiff')
 def main():
     print("Hello from python-project-50!")
 
@@ -9,5 +14,15 @@ def main():
     parser.add_argument("-f","--format", help="set format of output")
     parser.add_argument("first_file")
     parser.add_argument("second_file")
-    parser.parse_args()
+    args = parser.parse_args()
+    print(args)
+    print(args.first_file)
+    first_file = args.first_file
+    second_file = args.second_file
+    file1 = read_json(first_file)
+    file2 = read_json(second_file)
+    for col in file1:
+        print(col)
+    print(file1, file2)
+    
 
