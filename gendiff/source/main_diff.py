92ab52244ce3963d7798a43f247e313e93cf4f82
exolins <exolins@yandex.ru>
2026-04-17 15:40:27 +0300

    fix plain view again

--- gendiff/source/main_diff.py
+++ gendiff/source/main_diff.py
@@ -100,8 +100,18 @@ def make_diff(dict1, dict2):
     return result
 
 
+def plain_hook(data):
+    if isinstance(data, bool):
+        return "true" if data else "false"
+    if isinstance(data, NoneType):
+        return "null"
+    if isinstance(data, str):
+        return "'" + data + "'"
+    return data
+
+
 def complex_val(value):
-    return "[complex value]" if isinstance(value, dict) else f"{bool_hook(value)}"
+    return "[complex value]" if isinstance(value, dict) else f"{plain_hook(value)}"
 
 
 def plain_view(diff_value, parent=""):
@@ -118,12 +128,12 @@ def plain_view(diff_value, parent=""):
                     lines.append(
                         f"Property '{full_name}' "
                         + "was added with value: "
-                        + f"'{complex_val(value_inner['value'])}'"
+                        + f"{complex_val(value_inner['value'])}"
                     )
                 case "updated":
                     lines.append(
                         f"Property '{full_name}' was updated."
-                        + f" From '{complex_val(value_inner['old_value'])}' to '{complex_val(value_inner['new_value'])}'"
+                        + f" From {complex_val(value_inner['old_value'])} to {complex_val(value_inner['new_value'])}"
                     )
         else:
             lines.append(plain_view(value_inner["childrens"], full_name))
