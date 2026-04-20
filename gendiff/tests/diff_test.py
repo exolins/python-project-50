5876cc9ea7bf344813b9d3e53ec9cdf6a4493011
exolins <exolins@yandex.ru>
2026-04-20 17:38:31 +0300

    add test coverage

--- gendiff/tests/diff_test.py
+++ gendiff/tests/diff_test.py
@@ -2,6 +2,15 @@ import pytest
 
 from gendiff.source.main_diff import make_diff
 
+# from gendiff.source.file_tools import read_file
+from gendiff import generate_diff
+
+
+def read_file(file_path):
+    with open(file_path, encoding="utf-8") as file:
+        return file.read()
+
+
 param1 = ({}, {}, {})
 param2 = (
     {"one": "value1"},
@@ -99,3 +108,66 @@ param7 = (
 )
 def test_diff(data1, data2, data_result):
     assert make_diff(data1, data2) == data_result
+
+
+@pytest.mark.parametrize(
+    "file_path1, file_path2, expected_result",
+    [
+        (
+            "gendiff/tests/test_data/file1.json",
+            "gendiff/tests/test_data/file2.json",
+            "gendiff/tests/test_data/expected_result_json.txt",
+        ),
+        (
+            "gendiff/tests/test_data/file1.yaml",
+            "gendiff/tests/test_data/file2.yaml",
+            "gendiff/tests/test_data/expected_result_yaml.txt",
+        ),
+    ],
+)
+def test_generate_diff(file_path1, file_path2, expected_result):
+    diff = generate_diff(file_path1, file_path2)
+    expected = read_file(expected_result).strip()
+    assert diff.strip() == expected
+
+
+@pytest.mark.parametrize(
+    "file_path1, file_path2, expected_result",
+    [
+        (
+            "gendiff/tests/test_data/file1.json",
+            "gendiff/tests/test_data/file2.json",
+            "gendiff/tests/test_data/expected_result_plain.txt",
+        ),
+        (
+            "gendiff/tests/test_data/file1.yaml",
+            "gendiff/tests/test_data/file2.yaml",
+            "gendiff/tests/test_data/expected_result_plain.txt",
+        ),
+    ],
+)
+def test_generate_diff_plain(file_path1, file_path2, expected_result):
+    diff = generate_diff(file_path1, file_path2, format="plain")
+    expected = read_file(expected_result).strip()
+    assert diff.strip() == expected
+
+
+@pytest.mark.parametrize(
+    "file_path1, file_path2, expected_result",
+    [
+        (
+            "gendiff/tests/test_data/file1.json",
+            "gendiff/tests/test_data/file2.json",
+            "gendiff/tests/test_data/expected_result_json_format.txt",
+        ),
+        (
+            "gendiff/tests/test_data/file1.yaml",
+            "gendiff/tests/test_data/file2.yaml",
+            "gendiff/tests/test_data/expected_result_json_format.txt",
+        ),
+    ],
+)
+def test_generate_diff_json(file_path1, file_path2, expected_result):
+    diff = generate_diff(file_path1, file_path2, format="json")
+    expected = read_file(expected_result).strip()
+    assert diff.strip() == expected
